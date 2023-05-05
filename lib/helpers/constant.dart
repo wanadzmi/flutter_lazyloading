d37c7cbb7281e4ccee2a7b5e4c6bc3431757ab38
nadzmi <wannadzmi7@gmail.com>
2023-05-05 19:08:54 +0800

    added constant

---
+++ lib/helpers/constant.dart
@@ -0,0 +1,5 @@
+var baseURL = "https://api.themoviedb.org/3/movie/popular";
+var posterURL = "https://image.tmdb.org/t/p/w500/";
+
+// Please use your own API key. Available at: https://developers.themoviedb.org/
+var apiKey = "";
